2fdf91e951db580d7119977527ad665e71b8b693
18685030 <18685030@students.lincoln.ac.uk>
2019-07-02 16:03:32 +0100

    Updated styles and added new catagories

--- main.js
+++ main.js
@@ -3,38 +3,41 @@ $(document).ready(centerme);
 $(window).resize(centerme);
 
 function gather() {
+  finance = document.getElementById("finance").value;
+  finance = finance.replace(/\D/g,'');
+
   income = document.getElementById("income").value;
   income = income.replace(/\D/g,'');
+
   rent = document.getElementById("rent").value;
   rent = rent.replace(/\D/g,'');
-  utilities = document.getElementById("utilities").value;
-  utilities = utilities.replace(/\D/g,'');
+
   food = document.getElementById("food").value;
   food = food.replace(/\D/g,'');
-  insurance = document.getElementById("insurance").value;
-  insurance = insurance.replace(/\D/g,'');
-  result = income - rent - utilities - food - insurance;
-  savings = (income * 0.20);
+
+  utilities = document.getElementById("utilities").value;
+  utilities = utilities.replace(/\D/g,'');
+
+  phone = document.getElementById("social").value;
+  phone = phone.replace(/\D/g,'');
+
+  social = document.getElementById("social").value;
+  social = social.replace(/\D/g,'');
+
+  result = finance - income - rent - food - utilities - phone - social;
+  savings = income * 0.20;
+
   $(".results-data").empty();
-  $(".emoji").empty();
+
   if (result === 0) {
-    $(".results-data").append('<p class="text-danger convert-emoji"> You didn\'t enter anything. Try again ya filthy animal.</p>');
-    $(".emoji").append('<i class="frown">&nbsp;</i>');
+    $(".results-data").append('<p class="text-danger convert-emoji"> You didn\'t enter anything. Try again.</p>');
   }
   else if (result < 0) {
-    $(".results-data").append('<p class="text-danger"> After your expenses you have $' + result + ' left in your budget. You might want to try and reduce your spending this month.</p>');
-    $(".emoji").append('<i class="tear">&nbsp;</i>');
+    $(".results-data").append('<p class="text-danger"> After your expenses you have £' + result + ' left in your budget. You may want to review your budget plans.</p>');
   }
     else {
       $(".results-data").append(
-      '<p class="text-sucess"> After your expenses you have $' + result + ' left in your budget.</p>','<p class="text-sucess">But you should save at least $' + savings + '.</p>');
-      $(".emoji").append('<i class="happy">&nbsp;</i>');
+      '<p class="text-sucess"> After your expenses you have £' + result + ' left in your budget.</p>','<p class="text-sucess">If you can you should try to save $' + savings + ' for emergencies.</p>');
   }
 }
 
-function centerme() {
-boiheight = $(".center-meh-boi").height();
-middle = boiheight / 2;
-$(".center-meh-boi").css("margin-top","-" + middle + "px");
-console.log(boiheight);
-}
